2eba2a5691eb4d51dcc06281c612fae3df8214ea
Dexter Vin Catolos <dexter.catolos@oneresource.com.ph>
2023-08-17 10:15:37 +0800

    system configurations and update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFlightComponent } from './search-flight/search-flight.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFlightComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
