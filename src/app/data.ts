050d89eed46c20ab4fc46900a9fc9604097a0fa9
Dexter Vin Catolos <dexter.catolos@oneresource.com.ph>
2023-08-24 12:24:56 +0800

    Created data.ts for declaring static data

---
+++ src/app/data.ts
@@ -0,0 +1,9 @@
+export const airlineIdentification = [
+    {airlineID:"1",IATA_code:"IB",name:"Iberia",base_price:"500.99",economy:"10.99",premium_economy:"20.99",business:"30.99",first_class:"50.99",flight_date:"2023-09-04"},
+    {airlineID:"2",IATA_code:"BA",name:"British Airways",base_price:"300.99",economy:"5.90",premium_economy:"9.99",business:"12.99",first_class:"30.69",flight_date:"2023-09-08"},
+    {airlineID:"3",IATA_code:"LH",name:"Lufthansa",base_price:"200.09",economy:"10.08",premium_economy:"15.98",business:"20.10",first_class:"60.70",flight_date:"2023-09-06"},
+    {airlineID:"4",IATA_code:"FR",name:"Ryanair",base_price:"312.89",economy:"6.90",premium_economy:"20.90",business:"35.78",first_class:"60.00",flight_date:"2023-09-07"},
+    {airlineID:"5",IATA_code:"VY",name:"Vueling",base_price:"137.99",economy:"10.99",premium_economy:"15.99",business:"25.99",first_class:"55.99",flight_date:"2023-09-07"},
+    {airlineID:"6",IATA_code:"TK",name:"Turkish Airlines",base_price:"250.00",economy:"8.80",premium_economy:"18.90",business:"38.70",first_class:"58.90",flight_date:"2023-09-08"},
+    {airlineID:"7",IATA_code:"U2",name:"Easyjet",base_price:"100.99",economy:"10.89",premium_economy:"23.99",business:"34.88",first_class:"55.90",flight_date:"2023-09-08"}
+];
