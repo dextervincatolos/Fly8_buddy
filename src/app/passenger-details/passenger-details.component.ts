3aed70518a50b0d55ae80e30e6b1a193d6ae63e7
Dexter Vin Catolos <dexter.catolos@oneresource.com.ph>
2023-08-26 20:18:18 +0800

    passenger Details Update

--- src/app/passenger-details/passenger-details.component.ts
+++ src/app/passenger-details/passenger-details.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component , OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { PassengerInfoService } from '../passenger-info.service';
 import { ReservationService } from '../reservation.service';
 import { destination } from '../data';
@@ -11,21 +11,36 @@ import { destination } from '../data';
 })
 export class PassengerDetailsComponent{
 
- 
+  passengerForm!: FormGroup;  
 
   reservations: any[] = [];
 
-  constructor(private passengerInfoService: PassengerInfoService, private reservationService: ReservationService){
+  passenger: number = 0;
+  adultCount: number = 0; // Variable to store adult count
+  childrenCount: number = 0; // Variable to store children count
+
+  constructor(private fb: FormBuilder, private passengerInfoService: PassengerInfoService, private reservationService: ReservationService){
     
   }
 
   ngOnInit() {
 
-     
+   
 
-    //const selectedFlight = this.passengerInfoService.passengerInfo;
     this.reservations = this.reservationService.reservation;
 
+     // Access adult and children values and save to variables
+     this.adultCount = this.reservations.reduce((total, reservation) => total + reservation.adult, 0);
+     this.childrenCount = this.reservations.reduce((total, reservation) => total + reservation.children, 0);
+     this.passenger = this.adultCount + this.childrenCount;
+
+     this.passengerForm = this.fb.group({
+      passenger: this.passenger = this.adultCount + this.childrenCount,
+      names: this.fb.array([])
+    });
+
+    this.updateNameInputs(); // Initialize name inputs
+ 
     this.reservations = this.reservationService.reservation.map(reservation => {
       return {
         ...reservation,
@@ -33,11 +48,29 @@ export class PassengerDetailsComponent{
         cityCodeTo: this.getCityCode(reservation.cityCodeTo)
       };
     });
+    
 
    
-    
+   
+  }
+  get nameInputs() {
+    return this.passengerForm.get('names') as FormArray;
   }
 
+  updateNameInputs() {
+    const passenger = this.passengerForm.get('passenger')?.value || 0;
+    const nameInputs = this.nameInputs;
+
+    while (nameInputs.length !== passenger) {
+      if (nameInputs.length < passenger) {
+        nameInputs.push(this.fb.control(''));
+      } else {
+        nameInputs.removeAt(nameInputs.length - 1);
+      }
+    }
+  }
+
+ 
 
   getCityCode(cityID: string): string {
     const destinationItem = destination.find(dest => dest.cityID === cityID);
@@ -45,6 +78,10 @@ export class PassengerDetailsComponent{
   }
 
 
+  onSubmit() {
+    // Handle form submission
+    console.log(this.passengerForm.value.names);
+  }
 
 }
 
