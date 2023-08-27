b5cd086c821ba72bac0f2be0befe1db076cd0e93
Dexter Vin Catolos <dexter.catolos@oneresource.com.ph>
2023-08-27 12:39:43 +0800

    Searching flight Update

--- src/app/search-flight/search-flight.component.ts
+++ src/app/search-flight/search-flight.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BookFlightService } from '../book-flight.service';
 import { destination } from '../data';
 
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-search-flight',
@@ -13,9 +15,9 @@ export class SearchFlightComponent implements OnInit {
 
   destinations = destination;
 
-  flightSearchForm: FormGroup = new FormGroup({});
+  flightSearchForm!: FormGroup;
  
-  constructor(private formBuilder: FormBuilder, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private bookFlightService: BookFlightService) { }
 
   ngOnInit() {
     this.flightSearchForm = this.formBuilder.group({
@@ -25,23 +27,49 @@ export class SearchFlightComponent implements OnInit {
       departureDate: [''],
       returnDate: ['']
     });
+
+    // Disable returnDate control on initial load
+  this.flightSearchForm.get('returnDate')?.disable();
+
+          // Subscribe to changes in the flightType control
+  this.flightSearchForm.get('flightType')?.valueChanges.subscribe(value => {
+    
+    if (value === 'One-way') {
+      // Clear and disable the returnDate control
+      this.flightSearchForm.get('returnDate')?.disable();
+    } else {
+      // Enable the returnDate control and set Validators.required
+      this.flightSearchForm.get('returnDate')?.enable();
+    }
+    // Update the validation state
+    this.flightSearchForm.get('returnDate')?.updateValueAndValidity();
+  });
     
   }
 
-  shouldDisableReturnDate(): boolean {
-    return this.flightSearchForm?.get('flightType')?.value === 'One-way';
-  }
-  
+  // shouldDisableReturnDate(): boolean {
+  //   return this.flightSearchForm?.get('flightType')?.value === 'One-way';
+  // }
+
+
+
   setFlightType(type: string) {
     if (this.flightSearchForm) {
       this.flightSearchForm.get('flightType')?.setValue(type);
     }
   }
 
+  getCurrentDate(): string {
+    const today = new Date();
+    today.setDate(today.getDate() + 2); // Add one day
+    return today.toISOString().split('T')[0]; // Convert to YYYY-MM-DD format
+  }
+
+
 
   onSubmit() {
-    // Implement your form submission logic here
-    // this.router.navigate(['/Search_flight']);
-    this.router.navigate(['/Search_flight'], { queryParams: this.flightSearchForm.value });
+   const searchFlight= this.flightSearchForm.value;
+   this.bookFlightService.setData(searchFlight);
+   this.router.navigate(['/Search_flight']);
   }
 }
