584c2b778b442c0d4edeb73bf1f328ad566df59c
Dexter Vin Catolos <dexter.catolos@oneresource.com.ph>
2023-08-27 12:42:01 +0800

    Created book-flight and names services

---
+++ src/app/book-flight.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookFlightService {
+  private data: any;
+
+  setData(data: any) {
+    this.data = data;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  constructor() { }
+}
